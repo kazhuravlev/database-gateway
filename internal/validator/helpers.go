96a9d495c18abd01b1093f0f76aa0465efe76e91
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-11-02 17:03:02 +0100

    add useful helpers

--- internal/validator/helpers.go
+++ internal/validator/helpers.go
@@ -3,6 +3,8 @@ package validator
 import (
 	"fmt"
 	"github.com/auxten/postgresql-parser/pkg/sql/sem/tree"
+	"github.com/kazhuravlev/just"
+	"sort"
 )
 
 func getTableName(t tree.TableExpr) (string, error) {
@@ -14,4 +16,40 @@ func getTableName(t tree.TableExpr) (string, error) {
 	case *tree.AliasedTableExpr:
 		return t.String(), nil
 	}
+}
+
+func FilterType[T tree.NodeFormatter](req tree.NodeFormatter) ([]T, error) {
+	var res []T
+	err := Walk3(func(node tree.NodeFormatter) {
+		switch n := node.(type) {
+		case T:
+			res = append(res, n)
+		}
+	}, req)
+	if err := err; err != nil {
+		return nil, fmt.Errorf("filter statement: %w", err)
+	}
+
+	return res, nil
+}
+
+func GetColumnNames(req tree.NodeFormatter) ([]string, error) {
+	colItems, err := FilterType[*tree.ColumnItem](req)
+	if err != nil {
+		return nil, fmt.Errorf("filter columns: %w", err)
+	}
+
+	cols := just.SliceMap(colItems, func(col *tree.ColumnItem) string {
+		return col.String()
+	})
+
+	for _, col := range cols {
+		if col == "*" {
+			return nil, fmt.Errorf("unable to parse star notation: %w", ErrBadQuery)
+		}
+	}
+
+	sort.Strings(cols)
+
+	return cols, nil
 }
