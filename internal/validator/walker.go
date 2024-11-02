08bc6d7a5ab20d6d02283855bee8f3dc288dd642
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-11-02 15:57:39 +0100

    improve walking

--- internal/validator/walker.go
+++ internal/validator/walker.go
@@ -3,15 +3,27 @@ package validator
 import (
 	"errors"
 	"fmt"
-
 	"github.com/auxten/postgresql-parser/pkg/sql/parser"
+	"github.com/auxten/postgresql-parser/pkg/sql/sem/tree"
 	"github.com/kazhuravlev/database-gateway/internal/config"
+	"github.com/kazhuravlev/just"
+	"reflect"
+	"strings"
 )
 
 var ErrBadQuery = errors.New("bad query")
+var ErrComplicatedQuery = errors.New("complicated query")
+var ErrBadConfiguration = errors.New("bad configuration")
+var ErrAccessDenied = errors.New("access denied")
 
 func IsAllowed(target config.Target, user config.User, query string) error {
-	fmt.Println(query)
+	acls := just.SliceFilter(user.Acls, func(acl config.ACL) bool {
+		return acl.Target == target.Id
+	})
+	if len(acls) == 0 {
+		return fmt.Errorf("user have no any acls: %w", ErrAccessDenied)
+	}
+
 	stmts, err := parser.Parse(query)
 	if err != nil {
 		return fmt.Errorf("parse query: %w", err)
@@ -25,255 +37,351 @@ func IsAllowed(target config.Target, user config.User, query string) error {
 	case 1:
 	}
 
-	fmt.Println("=======================")
-	fmt.Println(len(stmts), stmts)
-	fmt.Println("=======================")
+	stmt := stmts[0]
 
-	//var statements []tree.Statement
-	//w := &walk.AstWalker{
-	//	Fn: func(ctx, node any) bool {
-	//		switch n := node.(type) {
-	//		case *tree.Select:
-	//			statements = append(statements, tree.Statement(n))
-	//		case *tree.Delete:
-	//			statements = append(statements, tree.Statement(n))
-	//		case *tree.Insert:
-	//			statements = append(statements, tree.Statement(n))
-	//		case *tree.Update:
-	//			statements = append(statements, tree.Statement(n))
-	//		}
-	//
-	//		return false
-	//	},
-	//}
-	//
-	//fmt.Println(w.Walk(stmts, nil))
-	//
-	//fmt.Println(statements)
-	//
-	//if len(statements) == 0 {
-	//	return false
-	//}
-	//
-	//return false
+	if tree.CanModifySchema(stmt.AST) {
+		return fmt.Errorf("unable to modify schema: %w", ErrBadQuery)
+	}
+
+	var crudRequests []tree.NodeFormatter
+	collect := func(n tree.NodeFormatter) {
+		switch n := n.(type) {
+		case *tree.Insert, *tree.Select, *tree.Update, *tree.Delete:
+			crudRequests = append(crudRequests, n)
+		}
+	}
+	if err := Walk3(collect, stmt.AST); err != nil {
+		return fmt.Errorf("walk statement ast: %w", err)
+	}
+
+	if len(crudRequests) == 0 {
+		return fmt.Errorf("unsupported query: %w", ErrBadQuery)
+	}
+
+	vectors := make([]IVector, 0, len(crudRequests))
+	for _, req := range crudRequests {
+		switch req := req.(type) {
+		default:
+			return fmt.Errorf("unexpected query: %w", ErrBadQuery)
+		case *tree.Insert:
+			vec, err := makeInsertVec(req)
+			if err != nil {
+				return fmt.Errorf("make insert vec: %w", err)
+			}
+			vectors = append(vectors, vec)
+		case *tree.Select:
+			vec, err := makeSelectVec(req)
+			if err != nil {
+				return fmt.Errorf("make select vec: %w", err)
+			}
+			vectors = append(vectors, vec)
+		case *tree.Update:
+			//vec, err := makeUpdateVec(req)
+			//if err != nil {
+			//	return fmt.Errorf("make update vec: %w", err)
+			//}
+			//vectors = append(vectors, vec)
+		case *tree.Delete:
+			//vec, err := makeDeleteVec(req)
+			//if err != nil {
+			//	return fmt.Errorf("make delete vec: %w", err)
+			//}
+			//vectors = append(vectors, vec)
+		}
+	}
+
+	// Find acl for each vector.
+	for _, vec := range vectors {
+		isAllowed := false
+		for _, acl := range acls {
+			if acl.Op != vec.Op() {
+				continue
+			}
+
+			if acl.Tbl != vec.Table() {
+				continue
+			}
+
+			isAllowed = acl.Allow
+			break
+		}
+
+		if !isAllowed {
+			return fmt.Errorf("denied operation (%s): %w", vec.String(), ErrAccessDenied)
+		}
+	}
 
 	return nil
 }
 
-//func Walk(stmts parser.Statements, fn func(parser.Statement) error) error {
-//	for _, stmt := range stmts {
-//		if err := fn(stmt); err != nil {
-//			return err
-//		}
-//	}
-//
-//
-//
-//	var walk func(...interface{})
-//	walk = func(nodes ...interface{}) {
-//		for _, node := range nodes {
-//			if _, ok := node.(tree.Datum); ok {
-//				continue
-//			}
-//
-//			switch node := node.(type) {
-//			case *tree.AliasedTableExpr:
-//				walk(node.Expr)
-//			case *tree.AndExpr:
-//				walk(node.Left, node.Right)
-//			case *tree.AnnotateTypeExpr:
-//				walk(node.Expr)
-//			case *tree.Array:
-//				walk(node.Exprs)
-//			case tree.AsOfClause:
-//				walk(node.Expr)
-//			case *tree.BinaryExpr:
-//				walk(node.Left, node.Right)
-//			case *tree.CaseExpr:
-//				walk(node.Expr, node.Else)
-//				for _, when := range node.Whens {
-//					walk(when.Cond, when.Val)
-//				}
-//			case *tree.RangeCond:
-//				walk(node.Left, node.From, node.To)
-//			case *tree.CastExpr:
-//				walk(node.Expr)
-//			case *tree.CoalesceExpr:
-//				for _, expr := range node.Exprs {
-//					walk(expr)
-//				}
-//			case *tree.ColumnTableDef:
-//			case *tree.ComparisonExpr:
-//				walk(node.Left, node.Right)
-//			case *tree.CreateTable:
-//				for _, def := range node.Defs {
-//					walk(def)
-//				}
-//				if node.AsSource != nil {
-//					walk(node.AsSource)
-//				}
-//			case *tree.CTE:
-//				walk(node.Stmt)
-//			case *tree.DBool:
-//			case tree.Exprs:
-//				for _, expr := range node {
-//					walk(expr)
-//				}
-//			case *tree.FamilyTableDef:
-//			case *tree.From:
-//				walk(node.AsOf)
-//				for _, table := range node.Tables {
-//					walk(table)
-//				}
-//			case *tree.FuncExpr:
-//				if node.WindowDef != nil {
-//					walk(node.WindowDef)
-//				}
-//				walk(node.Exprs, node.Filter)
-//			case *tree.IndexTableDef:
-//			case *tree.JoinTableExpr:
-//				walk(node.Left, node.Right, node.Cond)
-//			case *tree.NotExpr:
-//				walk(node.Expr)
-//			case *tree.NumVal:
-//			case *tree.OnJoinCond:
-//				walk(node.Expr)
-//			case *tree.Order:
-//				walk(node.Expr, node.Table)
-//			case tree.OrderBy:
-//				for _, order := range node {
-//					walk(order)
-//				}
-//			case *tree.OrExpr:
-//				walk(node.Left, node.Right)
-//			case *tree.ParenExpr:
-//				walk(node.Expr)
-//			case *tree.ParenSelect:
-//				walk(node.Select)
-//			case *tree.RowsFromExpr:
-//				for _, expr := range node.Items {
-//					walk(expr)
-//				}
-//			case *tree.Select:
-//				if node.With != nil {
-//					walk(node.With)
-//				}
-//				if node.OrderBy != nil {
-//					walk(node.OrderBy)
-//				}
-//				if node.Limit != nil {
-//					walk(node.Limit)
-//				}
-//				walk(node.Select)
-//			case *tree.Limit:
-//				walk(node.Count)
-//			case *tree.SelectClause:
-//				walk(node.Exprs)
-//				if node.Where != nil {
-//					walk(node.Where)
-//				}
-//				if node.Having != nil {
-//					walk(node.Having)
-//				}
-//				if node.DistinctOn != nil {
-//					for _, distinct := range node.DistinctOn {
-//						walk(distinct)
-//					}
-//				}
-//				if node.GroupBy != nil {
-//					for _, group := range node.GroupBy {
-//						walk(group)
-//					}
-//				}
-//				walk(&node.From)
-//			case tree.SelectExpr:
-//				walk(node.Expr)
-//			case tree.SelectExprs:
-//				for _, expr := range node {
-//					walk(expr)
-//				}
-//			case *tree.SetVar:
-//				for _, expr := range node.Values {
-//					walk(expr)
-//				}
-//			case *tree.StrVal:
-//			case *tree.Subquery:
-//				walk(node.Select)
-//			case tree.TableExprs:
-//				for _, expr := range node {
-//					walk(expr)
-//				}
-//			case *tree.TableName, tree.TableName:
-//			case *tree.Tuple:
-//				for _, expr := range node.Exprs {
-//					walk(expr)
-//				}
-//			case *tree.UnaryExpr:
-//				walk(node.Expr)
-//			case *tree.UniqueConstraintTableDef:
-//			case *tree.UnionClause:
-//				walk(node.Left, node.Right)
-//			case tree.UnqualifiedStar:
-//			case *tree.UnresolvedName:
-//			case *tree.ValuesClause:
-//				for _, row := range node.Rows {
-//					walk(row)
-//				}
-//			case *tree.Where:
-//				walk(node.Expr)
-//			case tree.Window:
-//				for _, windowDef := range node {
-//					walk(windowDef)
-//				}
-//			case *tree.WindowDef:
-//				walk(node.Partitions)
-//				if node.Frame != nil {
-//					walk(node.Frame)
-//				}
-//			case *tree.WindowFrame:
-//				if node.Bounds.StartBound != nil {
-//					walk(node.Bounds.StartBound)
-//				}
-//				if node.Bounds.EndBound != nil {
-//					walk(node.Bounds.EndBound)
-//				}
-//			case *tree.WindowFrameBound:
-//				walk(node.OffsetExpr)
-//			case *tree.With:
-//				for _, expr := range node.CTEList {
-//					walk(expr)
-//				}
-//			case *tree.Update:
-//				walk(node.Table)
-//				for _, expr := range node.Exprs {
-//					walk(expr)
-//				}
-//				for _, expr := range node.From {
-//					walk(expr)
-//				}
-//				for _, expr := range node.OrderBy {
-//					walk(expr)
-//				}
-//				if node.With != nil {
-//					walk(node.With)
-//				}
-//				if node.Limit != nil {
-//					walk(node.Limit)
-//				}
-//				if node.Where != nil {
-//					walk(node.Where)
-//				}
-//				if node.Returning != nil {
-//					walk(node.Returning)
-//				}
-//			default:
-//				if w.UnknownNodes != nil {
-//					w.UnknownNodes = append(w.UnknownNodes, node)
-//				}
-//			}
-//		}
-//	}
-//
-//
-//	return nil
+func getTableName(t tree.TableExpr) (string, error) {
+	switch t := t.(type) {
+	default:
+		return "", fmt.Errorf("query have complicated table name definition (%T): %w", t, ErrComplicatedQuery)
+	case *tree.TableName:
+		return t.FQString(), nil
+	case *tree.AliasedTableExpr:
+		return t.String(), nil
+	}
+}
+
+type IVector interface {
+	Op() config.Op
+	String() string
+	Table() string
+}
+
+type vecInsert struct {
+	tblName string
+}
+
+func (vecInsert) Op() config.Op {
+	return config.OpInsert
+}
+
+func (v vecInsert) String() string {
+	return "insert: " + v.tblName
+}
+
+func (v vecInsert) Table() string {
+	return v.tblName
+}
+
+type vecSelect struct {
+	tblName string
+	columns []string
+}
+
+func (vecSelect) Op() config.Op {
+	return config.OpSelect
+}
+
+func (v vecSelect) String() string {
+	return "select: " + v.tblName + " " + strings.Join(v.columns, ", ")
+}
+
+func (v vecSelect) Table() string {
+	return v.tblName
+}
+
+func makeInsertVec(req *tree.Insert) (*vecInsert, error) {
+	tName, err := getTableName(req.Table)
+	if err != nil {
+		return nil, fmt.Errorf("get table name for insert: %w", err)
+	}
+
+	return &vecInsert{tblName: tName}, nil
+}
+
+func makeSelectVec(req *tree.Select) (*vecSelect, error) {
+	sel, ok := req.Select.(*tree.SelectClause)
+	if !ok {
+		return nil, fmt.Errorf("query have complicated select definition: %w", ErrComplicatedQuery)
+	}
+
+	if len(sel.From.Tables) != 1 {
+		return nil, fmt.Errorf("select have a several tables: %w", ErrComplicatedQuery)
+	}
+
+	tName, err := getTableName(sel.From.Tables[0])
+	if err != nil {
+		return nil, fmt.Errorf("get table name for select: %w", err)
+	}
+
+	cols := make([]string, 0, len(sel.Exprs))
+	for _, col := range sel.Exprs {
+		name := col.Expr.String()
+		if name == "*" {
+			return nil, fmt.Errorf("unable to parse star notation: %w", ErrBadQuery)
+		}
+
+		cols = append(cols, name)
+	}
+
+	return &vecSelect{tblName: tName, columns: cols}, nil
+}
+
+//type WalkCtx struct {
+//	Filter  func(tree.NodeFormatter) bool
+//	Handler func(tree.NodeFormatter) error
 //}
+
+func Walk3(collect func(formatter tree.NodeFormatter), statements ...tree.NodeFormatter) error {
+	for _, stmt := range statements {
+		if stmt == nil {
+			continue
+		}
+
+		if val := reflect.ValueOf(stmt); val.IsZero() {
+			continue
+		}
+
+		collect(stmt)
+
+		var next []tree.NodeFormatter
+
+		switch node := stmt.(type) {
+		case *tree.With:
+			if node == nil {
+				continue
+			}
+
+			next = append(next, just.SliceMap(node.CTEList, func(cte *tree.CTE) tree.NodeFormatter {
+				return cte.Stmt
+			})...)
+		case *tree.Select:
+			next = append(next, node.With, node.Select, &node.OrderBy, node.Limit, &node.Locking)
+		case *tree.AliasedTableExpr:
+			next = append(next, node.Expr, node.IndexFlags)
+		case *tree.AndExpr:
+			next = append(next, node.Left, node.Right)
+		case *tree.AnnotateTypeExpr:
+			next = append(next, node.Expr)
+		case *tree.Array:
+			next = append(next, just.SliceMap(node.Exprs, func(expr tree.Expr) tree.NodeFormatter {
+				return expr
+			})...)
+		case *tree.AsOfClause:
+			next = append(next, node.Expr)
+		case *tree.BinaryExpr:
+			next = append(next, node.Left, node.Right)
+		case *tree.CaseExpr:
+			next = append(next, node.Expr)
+			next = append(next, just.SliceMap(node.Whens, func(w *tree.When) tree.NodeFormatter {
+				return w
+			})...)
+			next = append(next, node.Else)
+		case *tree.RangeCond:
+			next = append(next, node.Left, node.From, node.To)
+		case *tree.CastExpr:
+			next = append(next, node.Expr)
+		case *tree.CoalesceExpr:
+			next = append(next, just.SliceMap(node.Exprs, func(e tree.Expr) tree.NodeFormatter {
+				return e
+			})...)
+		case *tree.ComparisonExpr:
+			next = append(next, node.Left, node.Right)
+		case *tree.CreateTable:
+			next = append(next, just.SliceMap(node.Defs, func(t tree.TableDef) tree.NodeFormatter {
+				return t
+			})...)
+			next = append(next, node.AsSource)
+			next = append(next, &node.StorageParams)
+			next = append(next, node.PartitionBy)
+			next = append(next, node.Interleave)
+			next = append(next, &node.Table)
+		case *tree.Exprs:
+			next = append(next, just.SliceMap(*node, func(n tree.Expr) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.From:
+			next = append(next, &node.AsOf)
+			next = append(next, just.SliceMap(node.Tables, func(n tree.TableExpr) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.FuncExpr:
+			next = append(next, node.WindowDef, node.Filter, &node.Exprs)
+		case *tree.JoinTableExpr:
+			next = append(next, node.Left, node.Right, node.Cond)
+		case *tree.NotExpr:
+			next = append(next, node.Expr)
+		case *tree.OnJoinCond:
+			next = append(next, node.Expr)
+		case *tree.Order:
+			next = append(next, node.Expr, &node.Table)
+		case *tree.OrderBy:
+			next = append(next, just.SliceMap(*node, func(n *tree.Order) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.OrExpr:
+			next = append(next, node.Left, node.Right)
+		case *tree.ParenExpr:
+			next = append(next, node.Expr)
+		case *tree.ParenSelect:
+			next = append(next, node.Select)
+		case *tree.RowsFromExpr:
+			next = append(next, just.SliceMap(node.Items, func(n tree.Expr) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.Limit:
+			next = append(next, node.Offset, node.Count)
+		case *tree.SelectClause:
+			next = append(next, &node.Exprs, node.Where, node.Having, &node.DistinctOn, &node.GroupBy, &node.From)
+		case *tree.SelectExpr:
+			next = append(next, node.Expr)
+		case *tree.SelectExprs:
+			next = append(next, just.SliceMap(*node, func(n tree.SelectExpr) tree.NodeFormatter {
+				return &n
+			})...)
+		case *tree.SetVar:
+			next = append(next, &node.Values)
+		case *tree.Subquery:
+			next = append(next, node.Select)
+		case *tree.TableExprs:
+			next = append(next, just.SliceMap(*node, func(n tree.TableExpr) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.Tuple:
+			next = append(next, &node.Exprs)
+		case *tree.UnaryExpr:
+			next = append(next, node.Expr)
+		case *tree.UnionClause:
+			next = append(next, node.Left, node.Right)
+		case *tree.ValuesClause:
+			next = append(next, just.SliceMap(node.Rows, func(n tree.Exprs) tree.NodeFormatter {
+				return &n
+			})...)
+		case *tree.Where:
+			next = append(next, node.Expr)
+		case *tree.Window:
+			next = append(next, just.SliceMap(*node, func(n *tree.WindowDef) tree.NodeFormatter {
+				return n
+			})...)
+		case *tree.WindowDef:
+			next = append(next, &node.Partitions, node.Frame)
+		case *tree.WindowFrame:
+			next = append(next, node.Bounds.StartBound, node.Bounds.EndBound, node.Exclusion)
+		case *tree.WindowFrameBound:
+			next = append(next, node.OffsetExpr)
+		case *tree.Update:
+			next = append(next,
+				node.With,
+				node.Table,
+				&node.Exprs,
+				&node.From,
+				node.Where,
+				&node.OrderBy,
+				node.Limit,
+				node.Returning,
+			)
+		case *tree.DistinctOn:
+			next = append(next, just.SliceMap(*node, func(t tree.Expr) tree.NodeFormatter {
+				return t
+			})...)
+		case *tree.GroupBy:
+			next = append(next, just.Pointer(tree.Exprs(*node)))
+		case *tree.LockingClause:
+			next = append(next, just.SliceMap(*node, func(t *tree.LockingItem) tree.NodeFormatter {
+				return t
+			})...)
+		case *tree.ColumnTableDef:
+		case *tree.DBool:
+		case *tree.FamilyTableDef:
+		case *tree.IndexTableDef:
+		case *tree.NumVal:
+		case *tree.StrVal:
+		case *tree.TableName:
+		case *tree.UniqueConstraintTableDef:
+		case *tree.UnqualifiedStar:
+		case *tree.UnresolvedName:
+		default:
+			return fmt.Errorf("unknown node (%T): %w", stmt, ErrBadQuery)
+		}
+
+		if err := Walk3(collect, next...); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
