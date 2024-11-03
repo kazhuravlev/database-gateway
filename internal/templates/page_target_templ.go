2f6efa102f4c3e8c12b3b411b1e8de441d8deaa8
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-11-03 23:32:13 +0100

    generate templ

--- internal/templates/page_target_templ.go
+++ internal/templates/page_target_templ.go
@@ -63,7 +63,7 @@ func targetInfo(target config.Target) templ.Component {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var4 string
-			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(target.ID)
+			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(target.Id)
 			if templ_7745c5c3_Err != nil {
 				return templ.Error{Err: templ_7745c5c3_Err, FileName: `page_target.templ`, Line: 11, Col: 36}
 			}
