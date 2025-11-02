d7cb4bb92c9d179f971d4fc9bf21eb499e6f261e
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-11-02 01:19:51 +0100

    run codegen

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -1,3 +1,19 @@
+// Database Gateway provides access to servers with ACL for safe and restricted database interactions.
+// Copyright (C) 2024  Kirill Zhuravlev
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //nolint:testpackage
 package version
 
@@ -9,9 +25,13 @@ import (
 )
 
 func TestGetVersion(t *testing.T) {
+	t.Parallel()
+
 	require.Equal(t, "(devel)", GetVersion())
 
 	t.Run("returns explicitly set version when version variable is set", func(t *testing.T) {
+		t.Parallel()
+
 		// Save original value
 		original := version
 		defer func() { version = original }()
